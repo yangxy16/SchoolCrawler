1ada4dba589635e1d1d87a8cefc2792ae0c31f80
yangxy16 <yangxy16@lenovo.com>
2018-01-23 15:34:03 +0800

    update

---
+++ GKPATTR.py
@@ -0,0 +1,111 @@
+import requests as rq
+from bs4 import BeautifulSoup
+import random
+import re
+import json
+import time
+
+def getUA():
+    return random.choice( [
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+        "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+        "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+        "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+        "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+        "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+        "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+        "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 LBBROWSER",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+        "Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+        "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36"
+    ])
+    
+
+f = open( './sch.txt', 'r', encoding = 'utf-8' )
+fw = open( './schAttr.txt', 'w', encoding = 'utf-8' )
+
+while True:
+    line = f.readline()
+    if not line:
+        break
+    if len( line ) > 2:
+        obj = json.loads( line )
+        schId = obj['schoolId']
+        name = obj['name']
+        
+        schoolInfo = {}
+        
+        for i in range( 10 ):  
+            print( "正在抓取：" + str( schId ), i )
+            try:
+                time.sleep( random.randint( 1, 3 ) )
+                headers = { "User-Agent": getUA() }
+                r = rq.get( 'http://www.gaokaopai.com/daxue-jianjie-{}.html'.format( str( schId ) ), headers = headers, proxies = None )
+                
+                if r.status_code == 200:
+        
+                    schoolInfo['schoolId'] = schId
+                    schoolInfo['name'] = name
+                
+                    html = r.content.decode( 'utf-8' )
+                    html = html.replace( u'\u3000', u'' ).replace( u'\xa0', u'' ).replace( '\xa0', '' )
+                    
+                    schoolInfo['schoolAttr'] = { '211' : False, '985' : False, 'yan' : False, 'zizhu' : False, 'guofang' : False, 'zhuoyue' : False, 'sylsch' : False, 'sylxk': False }
+                    schoolInfo['enName'] = ''
+                    
+                    bsObj = BeautifulSoup( html, 'lxml' )
+                    for div in bsObj.find_all( 'div', class_ = 'schoolName' ):
+                        for subDiv in div.findAll( 'div', class_ = 'st' ):
+                            for img in subDiv.findAll( 'img' ):
+                                if img.attrs['title'] == '211':
+                                    schoolInfo['schoolAttr']['211'] = True
+                                elif img.attrs['title'] == '985':
+                                    schoolInfo['schoolAttr']['985'] = True
+                                elif img.attrs['title'] == '研究生院':
+                                    schoolInfo['schoolAttr']['yan'] = True
+                                elif img.attrs['title'] == '自主招生':
+                                    schoolInfo['schoolAttr']['zizhu'] = True
+                                elif img.attrs['title'] == '国防生':
+                                    schoolInfo['schoolAttr']['guofang'] = True
+                                elif img.attrs['title'] == '卓越计划':
+                                    schoolInfo['schoolAttr']['zhuoyue'] = True
+                                else:
+                                    print( str( img ) )
+                                
+                    try:    
+                        schoolInfo['enName'] = re.findall( r'<div class="enName">(.*?)</div>', html, re.I | re.M | re.S )[0]
+                    except:
+                        continue
+                    
+                    print( schoolInfo )
+                    fw.write( json.dumps( schoolInfo, ensure_ascii = False ) + '\r\n' )
+                    break
+                    
+            except:
+                continue
+
+f.close()
